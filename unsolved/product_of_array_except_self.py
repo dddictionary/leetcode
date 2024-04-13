8f0024f587348863a202f8aab688f8a9aaf5f8ac
Abrar Habib <abrarhabib285@gmail.com>
2024-04-13 19:41:13 -0400

    added product of array except self

--- unsolved/product_of_array_except_self.py
+++ unsolved/product_of_array_except_self.py
@@ -2,4 +2,13 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        pass
+        ans = []
+        for idx, num in enumerate(nums):
+            ans.append(self.get_product(nums.remove(num)))
+        return ans
+
+    def get_product(self, nums):
+        prod = 0
+        for num in nums:
+            prod *= num
+        return prod
